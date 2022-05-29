fb2fcdecfda995ee640cec0140adf5a1232c09d2
joseguillermo <joseguillermogonzalezzamora@gmail.com>
2022-05-29 19:31:08 -0300

    primer proyecto  de variables

---
+++ JavaScripts.js
@@ -0,0 +1,6 @@
+let number= 40;
+let numero= 16;
+let total = number + numero;
+prompt('ingrese primer valor:  ');
+prompt('ingrese segundo valor:  '); 
+document.write('el valor de '+number+ ' mas '+numero+ ' es el total de: ' +total);
